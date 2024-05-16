223c75b5a436f998ebefdefe3dc9e5a98059f9fd
cesar <cesarcachulo95@live.com.pt>
2024-05-16 19:15:15 +0100

    13. Using variables - String

--- 02-hello-world/Program.cs
+++ 02-hello-world/Program.cs
@@ -5,7 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("This time, I'm not using top level statements");
-        Console.ReadKey();
+        Console.WriteLine("This time, I'm not using top level statements\n");
+
+        // Declaring a string variable
+        string myFriendsName;
+        
+        // Assigning a value to that string variable
+        myFriendsName = "Frieren";
+        
+        // Using the variable
+        Console.WriteLine(myFriendsName);
+        
+        // Changing the variable's value
+        myFriendsName = "Stark";
+        
+        // Writing the variable's value to the console
+        Console.WriteLine(myFriendsName);
+        
+        // Declaring a new variable and assigning a value
+        string myThirdFriendsName = "Fern";
+        
+        // Writing the new  variable's value to the console
+        Console.WriteLine(myThirdFriendsName);
+
+        //Console.ReadKey();
     }
 }
