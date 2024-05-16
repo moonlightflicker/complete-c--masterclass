86b76723ad19e847ce1435113c057b112e7115db
cesar <cesarcachulo95@live.com.pt>
2024-05-16 21:00:39 +0100

    34. Explicit Conversion

--- 05-conversions/Program.cs
+++ 05-conversions/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Implicit conversion
         int myInt = 1231321321;
         double myDouble = myInt;
 
@@ -17,5 +18,27 @@ class Program
 
         float myFloat = 123.123f;
         myDouble = myFloat;
+        
+        
+        // Explicit conversion
+        // The number is different because it can't store all data, even though it "works"
+        int myInt2 = (int) myLong;
+        Console.WriteLine(myInt2);
+
+        // In this case it works fine because it can store all data
+        myLong = 123;
+        myInt2 = (int) myLong;
+        Console.WriteLine(myInt2);
+
+        // It can't store the whole number again
+        myDouble = 13.2123123123;
+        myFloat = (float)myDouble;
+        Console.WriteLine(myFloat);
+
+        // It can't store the whole number again
+        int myInt3;
+        double myDouble3 = 13.5;
+        myInt3 = (int)myDouble3;
+        Console.WriteLine(myInt3);
     }
 }
