e47dcad6d19160cf6f73760c0c542541812a98c9
cesar <cesarcachulo95@live.com.pt>
2024-05-16 20:55:24 +0100

    33. Implicit Conversion

---
+++ 05-conversions/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05_conversions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myInt = 1231321321;
+        double myDouble = myInt;
+
+        long myLong = myInt;
+
+        // Can't do this because myInt can't store as much data as myLong
+        //myInt = myLong;
+
+        myLong = 1231231263546548848;
+        //myInt = myLong;
+
+        float myFloat = 123.123f;
+        myDouble = myFloat;
+    }
+}
