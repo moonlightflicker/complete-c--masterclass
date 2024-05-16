26a9b9c85a08605b3607ddcccf840048879ffbf2
cesar <cesarcachulo95@live.com.pt>
2024-05-16 19:48:55 +0100

    Coding Exercise 1: Simple Variables Introduction

---
+++ 04-exercise-1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04_exercise_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintNumber();
+    }
+    
+    public static void PrintNumber()
+    {
+        int number = 10;
+        Console.WriteLine(number);
+    }
+}
