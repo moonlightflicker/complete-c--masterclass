a0c7a6c8f99a5d2970cba2de711e2f5e3776254e
cesar <cesarcachulo95@live.com.pt>
2024-05-16 19:22:47 +0100

    14. Understanding User Input and the Data Type String

---
+++ 03-my-addition-calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter something: ");
+
+string userValue = Console.ReadLine();
+
+Console.WriteLine("Value entered: " + userValue);
