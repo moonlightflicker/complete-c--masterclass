a4aa3cf44ed8d32bca5b8c745adf282e0adca372
cesar <cesarcachulo95@live.com.pt>
2024-05-16 19:03:09 +0100

    Top Level Statements

--- 01-hello-world/Program.cs
+++ 01-hello-world/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! How are you doing?");
 Console.WriteLine("I'm doing just fine, thanks!");
+Console.ReadKey();
---
+++ 02-hello-world/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _02_hello_world;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This time, I'm not using top level statements");
+        Console.ReadKey();
+    }
+}
