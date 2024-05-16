90866aaf7eb1fc8c4e03ff9e54fac558834a4669
cesar <cesarcachulo95@live.com.pt>
2024-05-16 19:53:58 +0100

    27. Using the Parse Method to Convert from String to Int

--- 03-my-addition-calculator/Program.cs
+++ 03-my-addition-calculator/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.Write("Enter something: ");
+Console.Write("Enter a whole number: ");
 
 // We can't use an int because Console.ReadLine() returns a string
-string userInput = Console.ReadLine();
+//string userInput = Console.ReadLine();
+
+// Parsing
+int userInput = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Value entered: " + userInput);
