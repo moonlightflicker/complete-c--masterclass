bd780ea578ff85c2bfaaedff11158b6a1b3799ec
cesar <cesarcachulo95@live.com.pt>
2024-05-17 19:33:43 +0100

    Start Coding Exercise 4: DataType Bool and Conditional Statements

---
+++ 12-exercise-4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _12_exercise_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CheckLogin();
+    }
+    
+    public static void CheckLogin()
+    {
+        string input = Console.ReadLine();
+        bool isLoggedIn = input != null && input.Length > 0;
+
+        if (!isLoggedIn)
+        {
+            Console.WriteLine("Welcome back!");
+        }
+        else
+        {
+            Console.WriteLine("Please log in.");
+        }
+    }
+}
