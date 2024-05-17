c7b098c47e7e82ac80852d9a2d8894fc8b561e58
cesar <cesarcachulo95@live.com.pt>
2024-05-17 19:11:38 +0100

    Coding Exercise 3: Special Characters and Escape Sequences

---
+++ 10-exercise-3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _10_exercise_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintSpecialCharacters();
+    }
+    
+    public static void PrintSpecialCharacters()
+    {
+        Console.WriteLine("Hello\n\tWorld!");
+    }
+}
