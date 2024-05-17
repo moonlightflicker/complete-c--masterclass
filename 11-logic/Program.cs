98ae1a01b25f27b223eb73aaa2162ac8b13f3cee
cesar <cesarcachulo95@live.com.pt>
2024-05-17 19:20:42 +0100

    48. If Statements

---
+++ 11-logic/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _11_logic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isRainy = false;
+        bool hasUmbrella = true;
+
+        if (isRainy)
+        {
+            Console.WriteLine("It's rainy");
+        }
+
+        Console.WriteLine("The program ran");
+    }
+}
