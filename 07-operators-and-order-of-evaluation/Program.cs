65709ff0326df362caec7fc621eb5facaac96d88
cesar <cesarcachulo95@live.com.pt>
2024-05-17 18:53:25 +0100

    37. Operators, order of Evaluation and some math basics

---
+++ 07-operators-and-order-of-evaluation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07_operators_and_order_of_evaluation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 6;
+        int num2 = 3;
+
+        // 53
+        Console.WriteLine("num1 + num2 = " + num1 + num2);
+        
+        // This is the right way
+        Console.WriteLine("num1 + num2 = " + (num1 + num2));
+        
+        // Not possible
+        //Console.WriteLine("num1 - num2 = " + num1 - num2);
+        // With bracket it is
+        Console.WriteLine("num1 - num2 = " + (num1 - num2));
+        
+        // Multiplication has priority
+        Console.WriteLine("num1 * num2 = " + num1 * num2);
+        
+        // Division has priority
+        Console.WriteLine("num1 / num2 = " + num1 / num2);
+    }
+}
