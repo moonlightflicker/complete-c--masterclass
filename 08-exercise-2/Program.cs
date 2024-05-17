73ee72122338549e9800f2e2209920d1f0c656da
cesar <cesarcachulo95@live.com.pt>
2024-05-17 18:56:56 +0100

    Start Coding Exercise 2: Fixing Simple Bugs

---
+++ 08-exercise-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Channels;
+
+namespace _08_exercise_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalculateAverage();
+    }
+    
+    public static void CalculateAverage()
+    {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int average = (num1 + num2 + num3) / 3;
+        Console.WriteLine("The average is: " + average);
+    }
+}
