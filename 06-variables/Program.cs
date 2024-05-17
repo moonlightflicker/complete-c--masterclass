9c4ecd1077a72e5491a88f9aab173dc89bcc7478
cesar <cesarcachulo95@live.com.pt>
2024-05-17 18:47:30 +0100

    36. Implicitly Typed Variables vs Explicitly Typed Variables

---
+++ 06-variables/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06_variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Implicitly typed variable
+        var myFavouriteGenre = "rock";
+        var myFavouriteNumber = 7;
+        
+        // Explicitly types variable
+        string myFavouriteMovie = "Die Hard";
+        int yourFavouriteNumber = 3;
+        
+        // This does not work
+        //myFavouriteGenre = yourFavouriteNumber;
+
+
+
+        var sum = myFavouriteNumber + yourFavouriteNumber;
+        Console.WriteLine(sum);
+    }
+}
