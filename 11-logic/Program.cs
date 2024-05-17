7c494c9816369ce645c75f70e1c1aa17f19516ac
cesar <cesarcachulo95@live.com.pt>
2024-05-17 19:27:27 +0100

    51. Relational operators

--- 11-logic/Program.cs
+++ 11-logic/Program.cs
@@ -24,5 +24,20 @@ class Program
         }
 
         Console.WriteLine("The program ran");
+        
+        
+        
+        
+        int num1 = 5;
+        int num2 = 6;
+        
+        // Relational operator
+        bool isGreater = num1 > num2;
+
+        int age = 35;
+        if (age >= 18)
+        {
+            Console.WriteLine("Party Time!");
+        }
     }
 }
